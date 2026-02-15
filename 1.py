46c0215046f5ff63ed7819202ba8a2111d4fb158
Bayana07 <bonita.carreraa@gmail.com>
2026-02-15 11:10:52 +0600

    pr2

--- 1.py
+++ 1.py
@@ -1,4 +1,27 @@
-a = input()
-b = input()
+# n = int(input())
+# l = int(input())
+# r = int(input())
+# li = list(map(int, input().split()))
+
+# start = l-1
+# end = r
+# res = li[:start] + li[start:end][::-1]+li[end:]
+# print(*res)
+n = int(input())
+l = input().split()
+d = dict()
+un = l[0]
+for i in range(n):
+    # name = input()
+    
+    if l[i] in d:
+        d[l[i]] += 1
+    else:
+        d[l[i]] = 1
+    
+    print(d.values, d.keys)
+
+        
+        
 
 
