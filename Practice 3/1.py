97fd4559d4916cf9517c1779777c0b8f449dd7fe
Bayana07 <bonita.carreraa@gmail.com>
2026-04-02 22:43:38 +0600

    pr7

--- Practice 3/1.py
+++ Practice 3/1.py
@@ -1,10 +1,35 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+import json
+import re
 
-p1 = Person("Tobias", 25)
-print(p1.age)
 
-p1.age = 26
-print(p1.age)
+data = '{"Ivan": "123@gmail.com", "Serega": "lol_mail"}'
+
+p = json.loads(data)
+for name, em in p.items():
+    if re.match(r"\w+@\w+.\w+", em):
+        print(name)
+
+
+# from datetime import datetime, timedelta
+# d_str = input()
+# n = int(input())
+# d = datetime.strptime(d_str, "%Y-%m-%d")
+# print((d - timedelta(days=n)).date())
+
+
+# from datetime import datetime
+# st = datetime.strptime(input(), "%Y-%m-%d")
+# for _ in range(3):
+#     oth = datetime.strptime(input(), "%Y-%m-%d")
+#     diff = st - oth
+#     print(diff.days)
+
+
+
+# n = input()
+# gen = sq(10000000)
+# for _ in range(n-1):
+#     print(next(gen), end=',')
+# print(next(gen))
+# print(','.join(map(str, sq(n))))
+
