772b68116242ffaa2da79e18c8d43fef1d323745
Bayana07 <bonita.carreraa@gmail.com>
2026-05-02 12:20:50 +0600

    tsi

--- TSIS/TSIS1/connect.py
+++ TSIS/TSIS1/connect.py
@@ -6,25 +6,5 @@ def get_con():
 
 
 
-# import sys
-# import os
 
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# from config import load_config
-
-# import psycopg2
-# from config import load_config
-
-# def get_con(config):
-#     try:
-#         with psycopg2.connect(**config) as conn:
-#             print('Connected to the PostgreSQL server.')
-#             return conn
-#     except (psycopg2.DatabaseError, Exception) as error:
-#         print(error)
-
-# if __name__ == '__main__':
-#     config = load_config()
-#     get_con(config)
 
